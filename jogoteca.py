9de94cde1c4d5659c35503aa73db8aa8a91b025f
cesar-spaziante <cesar.spaziante@gmail.com>
2022-06-12 18:04:54 -0300

    Flask - Autenticação | Rotas | Flash | Url_for

--- jogoteca.py
+++ jogoteca.py
@@ -1,7 +1,12 @@
 #render_template renderiza nossos arquivos .HTML
 #request coleta as informações do nosso formulário
+#session permite que retenhamos informações pelos cookies do navegador
+#flash serve para mostramos mensagens na tela para o usuário
+#url_for dinamiza as nossas referencias às rotas
 
-from flask import Flask, redirect, render_template, request, redirect
+
+from unicodedata import category
+from flask import Flask, flash, redirect, render_template, request, redirect, session, flash, url_for
 
 #Criando uma classe para os jogos
 class Jogo:
@@ -9,6 +14,20 @@ class Jogo:
         self.nome=nome
         self.categoria=categoria
         self.console=console
+
+#Criando uma classe para os usuarios
+class Usuario:
+    def __init__(self, nome, nickname, senha):
+        self.nome=nome
+        self.nickname=nickname
+        self.senha=senha
+
+usuario1 = Usuario('Geralt', 'lobo_branco', 'alohomora')
+usuario2 = Usuario('Yennefer', 'yen', 'teste')
+usuario3 = Usuario('Ciri', 'cintra_leon', 'winter')
+usuarios = { usuario1.nickname : usuario1,
+             usuario2.nickname : usuario2,
+             usuario3.nickname : usuario3 }
 
 #Deixando a lista de jogos global para todas as funções usarem
 jogo1=Jogo('The Witcher 3','Action RPG','PS4')
@@ -18,6 +37,10 @@ lista_jogos = [jogo1,jogo2,jogo3]
 
 #Criando o aoo em Flask
 app = Flask(__name__) #__name__ se refere ao proprio arquivo
+
+#Criando criptrografia do site
+app.secret_key = "A1ur4"
+
 
 #Rota padrão
 @app.route('/')
@@ -29,6 +52,8 @@ def index():
 #Form de novo jogo
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
     return render_template('novo_jogo.html', titulo='Cadastro de Jogo')
 
 #Criação de um novo jogo
@@ -40,6 +65,33 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome,categoria,console) #Chamando a classe Jogo para cirar um novo jogo
     lista_jogos.append(jogo) #Append do novo jogo criado para a lista de jogos
-    return redirect('/') #Redireciona para a rota "/"
+    return redirect(url_for('index')) #Redireciona para a rota "/"
+
+
+@app.route('/login', methods=['GET',])
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', titulo='Faça se Login', proxima=proxima)    
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash('Bem-vindo {}!'.format(session['usuario_logado']), 'alert alert-success')
+            proxima_pag = request.form['proxima']
+            return redirect(proxima_pag)
+
+    else:
+        flash('{} não existe!'.format(request.form['usuario']), 'alert alert-warning')
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None 
+    flash('Logout efetuado com sucesso!', 'alert alert-success')
+    return redirect(url_for('login'))
 
 app.run(debug=True) #debug faz com que nossa aplicação entende e atualize alterações em tempo real
